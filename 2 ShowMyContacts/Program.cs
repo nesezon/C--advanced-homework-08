805cfe5315c95aff4fabb5694b07bcbbe8ce0f0b
nesezon <tilichenko@gmail.com>
2021-05-11 23:26:23 +0300

    Задание 2. ShowMyContacts

---
+++ 2 ShowMyContacts/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ShowMyContacts {
+  static class Program {
+    static void Main() {
+      string file2search = "TelephoneBook.xml";
+      // ищу файл в папке 1-го упражнения
+      DirectoryInfo directoryInfo = new DirectoryInfo("../../../1 CreateMyContacts");
+      FileInfo[] fileNames = directoryInfo.GetFiles(file2search, SearchOption.AllDirectories);
+      if (fileNames.Length > 0) {
+        // найден
+        XDocument xdoc = XDocument.Load(fileNames[0].FullName);
+        IEnumerable<Contact> contacts = from row in xdoc.Element("MyContacts")?.Elements("Contact")
+                                        select new Contact {
+                                          Name = row.Value,
+                                          TelephoneNumber = row.Attribute("TelephoneNumber")?.Value
+                                        };
+        foreach (Contact contact in contacts) {
+          Console.WriteLine(contact);
+        }
+
+        //задержка
+        Console.ReadKey();
+      } else {
+        // не найден
+        Console.WriteLine($"Файл {file2search} не найден в папке {directoryInfo.FullName}");
+        Console.WriteLine("(Чтобы он появился, запустите исполняемый файл первого упражнения)");
+      }
+    }
+  }
+
+  /// <summary>
+  /// Класс для представления контакта
+  /// </summary>
+  class Contact {
+    public string Name { get; set; }
+    public string TelephoneNumber { get; set; }
+    public override string ToString() {
+      return string.Format(format: "{0, -20} {1}",arg0: Name, TelephoneNumber);
+    }
+  }
+}
