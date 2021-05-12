2d7112ecf9c3d7a778759c28d9c1cfedf910677d
nesezon <tilichenko@gmail.com>
2021-05-12 22:57:18 +0300

    Задание 3. ShowContactPhonesOnly

---
+++ 3 ShowContactPhonesOnly/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ShowContactPhonesOnly {
+  class Program {
+    static void Main(string[] args) {
+      string file2search = "TelephoneBook.xml";
+      // ищу файл в папке 1-го упражнения
+      DirectoryInfo directoryInfo = new DirectoryInfo("../../../1 CreateMyContacts");
+      FileInfo[] fileNames = directoryInfo.GetFiles(file2search, SearchOption.AllDirectories);
+      if (fileNames.Length > 0) {
+        XDocument xdoc = XDocument.Load(fileNames[0].FullName);
+        
+        // извлечение номеров телефонов
+        IEnumerable<string> Phones = from num in xdoc.Element("MyContacts")?.Elements("Contact")
+          select num.Attribute("TelephoneNumber")?.Value;
+
+        // вывод на экран
+        foreach (var phone in Phones) {
+          Console.WriteLine(phone);
+        }
+
+        //задержка
+        Console.ReadKey();
+      } else {
+        // не найден
+        Console.WriteLine($"Файл {file2search} не найден в папке {directoryInfo.FullName}");
+        Console.WriteLine("(Чтобы он появился, запустите исполняемый файл первого упражнения)");
+      }
+    }
+  }
+}
